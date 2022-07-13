cb23857562a6e396c9bf431e42b2da27815b8de5
SweetBubaleXXX <1pcpcpc1pc@gmail.com>
2022-07-13 20:39:45 +0300

    Tests for asymmetric key

--- tests/test.py
+++ tests/test.py
@@ -13,7 +13,7 @@ class TestSymmetricKey(unittest.TestCase):
 
     def test_encryption(self):
         encrypted_data = self.key.encrypt(b"byte data")
-        self.assertTrue(isinstance(encrypted_data, bytes))
+        self.assertIsInstance(encrypted_data, bytes)
 
     def test_decryption(self):
         data = b"byte data"
@@ -27,6 +27,43 @@ class TestSymmetricKey(unittest.TestCase):
         new_key = symmetric.SymmetricKey(key, iv)
         data = b"byte data"
         self.assertEqual(self.key.encrypt(data), new_key.encrypt(data))
+
+
+class TestAsymmetricKey(unittest.TestCase):
+    def setUp(self):
+        self.key_size = asymmetric.PrivateKey.key_sizes[1]
+        self.private_key = asymmetric.PrivateKey.generate(self.key_size)
+
+    def test_key_size_method(self):
+        self.assertEqual(self.private_key.key_size, self.key_size)
+
+    def test_public_key_creation(self):
+        public_key = self.private_key.public_key
+        self.assertIsInstance(public_key, asymmetric.PublicKey)
+
+    def test_private_key_serialization(self):
+        serialized_data = self.private_key.serialize(b"password")
+        loaded_key = asymmetric.PrivateKey.load(serialized_data, b"password")
+        self.assertIsInstance(loaded_key, asymmetric.PrivateKey)
+
+    def test_public_key_serialization(self):
+        public_ley = self.private_key.public_key
+        serialized_data = public_ley.serialize()
+        loaded_key = asymmetric.PublicKey.load(serialized_data)
+        self.assertIsInstance(loaded_key, asymmetric.PublicKey)
+
+    def test_decryption(self):
+        data = b"byte data"
+        encrypted_data = self.private_key.encrypt(data)
+        decrypted_data = self.private_key.decrypt(encrypted_data)
+        self.assertEqual(decrypted_data, data)
+
+    def test_public_encryption(self):
+        data = b"byte data"
+        public_key = self.private_key.public_key
+        encrypted_data = public_key.encrypt(data)
+        decrypted_data = self.private_key.decrypt(encrypted_data)
+        self.assertEqual(decrypted_data, data)
 
 
 if __name__ == "__main__":
