e55452a95c616b6ed03e5a1388d276c29d627490
termux <1pcpcpc1pc@gmail.com>
2022-07-13 13:18:48 +0300

    Package name and requirements in setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name="KEK",
+setup(name="kek",
       version="0.1.0",
       description="Kinetic Effective Key",
       url="https://github.com/SweetBubaleXXX/KEK.git",
       author="SweetBubaleXXX",
       author_email="1pcpcpc1pc@gmail.com",
       license="GNU General Public License v3.0",
-      packages=["KEK"],
-      install_requires=["cryptography"],
+      packages=find_packages(include=["KEK"]),
+      install_requires=[
+          "cryptography>=36.0.0"
+      ],
       python_requires=">=3.7")
+
