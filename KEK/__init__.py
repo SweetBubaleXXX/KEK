22c6fc8e1fa25e3082ec9ec2ca628599eb63381d
SweetBubaleXXX <1pcpcpc1pc@gmail.com>
2022-07-15 18:17:06 +0300

    Version changed to 0.2.1

--- KEK/__init__.py
+++ KEK/__init__.py
@@ -1,3 +1,3 @@
 from .hybrid import PrivateKEK, PublicKEK
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
--- setup.py
+++ setup.py
@@ -1,17 +1,35 @@
 from setuptools import setup, find_packages
 
 setup(name="gnukek",
-      version="0.2.0",
+      version="0.2.1",
       description="Kinetic Effective Key",
-      url="https://github.com/SweetBubaleXXX/KEK",
-      project_urls={
-          "Bug Tracker": "https://github.com/SweetBubaleXXX/KEK/issues",
-      },
       author="SweetBubaleXXX",
       license="GNU General Public License v3.0",
+      url="https://github.com/SweetBubaleXXX/KEK",
+      project_urls={
+          "Source": "https://github.com/SweetBubaleXXX/KEK",
+          "Bug Tracker": "https://github.com/SweetBubaleXXX/KEK/issues",
+      },
+      classifiers=[
+          "DEVELOPMENT STATUS :: 3 - ALPHA",
+          "TOPIC :: SECURITY :: CRYPTOGRAPHY",
+          "Programming Language :: Python :: 3",
+          "LICENSE :: OSI APPROVED :: GNU GENERAL PUBLIC LICENSE V3 (GPLV3)",
+          "Operating System :: OS Independent",
+      ],
       packages=find_packages(include=["KEK"]),
       install_requires=[
           "cryptography>=35.0.0"
       ],
+      extras_require={
+          "dev": [
+              "mypy",
+              "pycodestyle"
+          ],
+          "build": [
+              "build",
+              "twine"
+          ]
+      },
       python_requires=">=3.7",
       test_suite="tests")
