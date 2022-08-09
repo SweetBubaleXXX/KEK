391bb9a9cce21778729656d45db3d26251de7a49
SweetBubaleXXX <1pcpcpc1pc@gmail.com>
2022-08-09 12:53:11 +0300

    SymmetricKey chunk encryption/decryption support

--- KEK/symmetric.py
+++ KEK/symmetric.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 import os
-from typing import Type
+from typing import Optional, Type, Union
 
 from cryptography.hazmat.primitives.ciphers import Cipher, modes
 from cryptography.hazmat.primitives.ciphers.algorithms import AES
@@ -48,7 +48,8 @@ class SymmetricKey(BaseSymmetricKey):
                 f"Should be one of {self.key_sizes}.")
         self._key = key
         self._iv = iv
-        self._cipher = Cipher(AES(key), modes.CBC(iv))
+        self._prev_encryption_block: Union[bytes, None] = None
+        self._prev_decryption_block: Union[bytes, None] = None
 
     @property
     def key_size(self) -> int:
@@ -64,6 +65,46 @@ class SymmetricKey(BaseSymmetricKey):
     def iv(self) -> bytes:
         """Byte data of iv."""
         return self._iv
+
+    def __create_cipher(self, key: bytes, iv: bytes) -> Cipher:
+        """Create Cipher object."""
+        return Cipher(AES(key), modes.CBC(iv))
+
+    def __add_padding(self, data: bytes) -> bytes:
+        """Add padding to byte data with uncertain length."""
+        padder = PKCS7(self.block_size).padder()
+        return padder.update(data) + padder.finalize()
+
+    def __remove_padding(self, padded_data: bytes) -> bytes:
+        """Return unpadded byte data."""
+        unpadder = PKCS7(self.block_size).unpadder()
+        return unpadder.update(padded_data) + unpadder.finalize()
+
+    def __encrypt_raw(self, data: bytes,
+                      prev_block: Optional[bytes] = None) -> bytes:
+        """Encrypt byte data using cipher.
+
+        Length of data must be multiple of block size.
+        Previous block is used when creating cipher.
+        If no previous block specified, iv is used.
+
+        """
+        cipher = self.__create_cipher(self._key, prev_block or self._iv)
+        encryptor = cipher.encryptor()
+        return encryptor.update(data) + encryptor.finalize()
+
+    def __decrypt_raw(self, encrypted_data: bytes,
+                      prev_block: Optional[bytes] = None) -> bytes:
+        """Decrypt byte data using cipher.
+
+        Length of data must be multiple of block size.
+        Previous block is used when creating cipher.
+        If no previous block specified, iv is used.
+
+        """
+        cipher = self.__create_cipher(self._key, prev_block or self._iv)
+        decryptor = cipher.decryptor()
+        return decryptor.update(encrypted_data) + decryptor.finalize()
 
     @classmethod
     @raises(exceptions.KeyGenerationError)
@@ -88,27 +129,6 @@ class SymmetricKey(BaseSymmetricKey):
         return cls(key, iv)
 
     @raises(exceptions.EncryptionError)
-    def encrypt_raw(self, data: bytes) -> bytes:
-        """Encrypt byte data without adding padding.
-
-        Parameters
-        ----------
-        data : bytes
-            Byte data to encrypt.
-            Length of input bytes must be multiple of block's length.
-
-        Returns
-        -------
-        Encrypted bytes.
-
-        Raises
-        ------
-        EncryptionError
-        """
-        encryptor = self._cipher.encryptor()
-        return encryptor.update(data) + encryptor.finalize()
-
-    @raises(exceptions.EncryptionError)
     def encrypt(self, data: bytes) -> bytes:
         """Encrypt byte data with adding padding.
 
@@ -125,29 +145,42 @@ class SymmetricKey(BaseSymmetricKey):
         ------
         EncryptionError
         """
-        padder = PKCS7(self.block_size).padder()
-        padded_data = padder.update(data) + padder.finalize()
-        return self.encrypt_raw(padded_data)
+        padded_data = self.__add_padding(data)
+        return self.__encrypt_raw(padded_data)
 
-    @raises(exceptions.DecryptionError)
-    def decrypt_raw(self, encrypted_data: bytes) -> bytes:
-        """Decrypt byte data without unpadding it.
+    @raises(exceptions.EncryptionError)
+    def encrypt_chunk(self, data: bytes, is_last: bool = False) -> bytes:
+        """Encrypt piece of data.
+
+        Caches last block of encrypted data,
+        which will be used in the following call.
+        Multiple calls will generate sequence of connected chunks.
+        Sequence of chunks can be decrypted as entirely encrypted byte data.
 
         Parameters
         ----------
-        encrypted_data : bytes
-            Byte data to decrypt.
-
+        data : bytes
+            Byte data to encrypt.
+            Length of input bytes must be multiple of block's length.
+        is_last : bool
+            If True, adds padding to the last block of data, and
+            the following call will use iv instead of previous block.
         Returns
         -------
-        Decrypted bytes.
+        Encrypted bytes.
 
         Raises
         ------
-        DecryptionError
+        EncryptionError
         """
-        decryptor = self._cipher.decryptor()
-        return decryptor.update(encrypted_data) + decryptor.finalize()
+        if is_last:
+            data = self.__add_padding(data)
+        encrypted_data = self.__encrypt_raw(data, self._prev_encryption_block)
+        if is_last:
+            self._prev_encryption_block = None
+        else:
+            self._prev_encryption_block = encrypted_data[self.block_size//8:]
+        return encrypted_data
 
     @raises(exceptions.DecryptionError)
     def decrypt(self, encrypted_data: bytes) -> bytes:
@@ -166,6 +199,42 @@ class SymmetricKey(BaseSymmetricKey):
         ------
         DecryptionError
         """
-        unpadder = PKCS7(self.block_size).unpadder()
-        decrypted_data = self.decrypt_raw(encrypted_data)
-        return unpadder.update(decrypted_data) + unpadder.finalize()
+        decrypted_data = self.__decrypt_raw(encrypted_data)
+        return self.__remove_padding(decrypted_data)
+
+    @raises(exceptions.DecryptionError)
+    def decrypt_chunk(self, encrypted_data: bytes,
+                      is_last: bool = False) -> bytes:
+        """Decrypt piece of data.
+
+        Caches last block of encrypted data,
+        which will be used in the following call.
+        All pieces must be parts of encrypted data
+        and be given in the correct order.
+
+        Parameters
+        ----------
+        encrypted_data : bytes
+            Byte data to decrypt.
+            Length of input bytes must be multiple of block's length.
+        is_last : bool
+            If True, removes padding from the last block of data, and
+            the following call will use iv instead of previous block.
+
+        Returns
+        -------
+        Decrypted bytes.
+
+        Raises
+        ------
+        DecryptionError
+        """
+        decrypted_data = self.__decrypt_raw(
+            encrypted_data,
+            self._prev_decryption_block
+        )
+        if is_last:
+            self._prev_decryption_block = None
+            return self.__remove_padding(decrypted_data)
+        self._prev_decryption_block = decrypted_data[self.block_size//8:]
+        return decrypted_data
